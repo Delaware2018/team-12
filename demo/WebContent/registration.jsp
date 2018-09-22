e38a532b45ca2e1aa8e612b831f383728ae9a9fb
aeged <gednov@usc.edu>
2018-09-22 02:13:25 -0400

    made servlet for registration page

--- demo/WebContent/registration.jsp
+++ demo/WebContent/registration.jsp
@@ -5,17 +5,20 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Registration</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-	<div class="body-wrapper">
-		<form class="form" method="POST" name="registration-form">
+	<div id="body-wrapper" class="container containder-fluid my-5">
+		<form class="form" method="POST" name="registration-form" action="Registration">
 			<div class="form-group">
 				<label for="name">Name: </label>
 				<input type="text" name="name" id="name" placeholder="name"> 
+				<span id="name_err" style="color:red">${name_err!=null? name_err : ''}</span>
 			</div>
 			<div class="form-group">
 				<label for="age">Age: </label>
-				<input type="text" name="age" id="age" placeholder="age"> 
+				<input type="text" name="age" id="age" placeholder="age">
+				<span id="age_err" style="color:red">${age_err!=null? age_err : ''}</span>
 			</div>
 			<div class="form-group">
 				<label for="gender">Gender: </label> <select
@@ -24,18 +27,22 @@
 					<option>Male</option>
 					<option>Female</option>
 				</select>
+				<span id="gender_err" style="color:red">${gender_err!=null? gender_err : ''}</span>
 			</div>
 			<div class="form-group">
-				<label for="name">Email: </label>
+				<label for="email">Email: </label>
 				<input type="text" name="email" id="email" placeholder="email"> 
+				<span id="email_err" style="color:red">${email_err!=null? email_err : ''}</span>
 			</div>
 			<div class="form-group">
-				<label for="name">Phone: </label>
+				<label for="phone">Phone: </label>
 				<input type="text" name="phone" id="phone" placeholder="phone"> 
+				<span id="phone_err" style="color:red">${phone_err!=null? phone_err : ''}</span>
 			</div>
 			<div class="form-group">
-				<label for="name">Zip: </label>
+				<label for="zip">Zip: </label>
 				<input type="text" name="zip" id="zip" placeholder="zip"> 
+				<span id="zip_err" style="color:red">${zip_err!=null? zip_err : ''}</span>
 			</div>
 			<!-- bottom submit and reset button -->
 			<input type="submit" class="btn btn-primary"></input>
---
+++ demo/src/main/java/Registration.java
@@ -0,0 +1,78 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/Registration")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String zip = request.getParameter("zip");
+		
+		// test data coming in 
+    	String next = "/index.jsp";
+    	if (name == "") {
+    		request.setAttribute("name_err", "please enter your name");
+    		next = "/registration.jsp";
+    	}
+    	if (age == ""){
+    		request.setAttribute("age_err", "please enter your age");
+    		next = "/registration.jsp";
+    	}
+    	if (gender == null) {
+    		request.setAttribute("gender_err", "please select your gender");
+    		next = "/registration.jsp";
+    	}
+    	if (email == ""){
+    		request.setAttribute("email_err", "please enter your email address");
+    		next = "/registration.jsp";
+    	}
+    	if (phone == ""){
+    		request.setAttribute("phone_err", "please enter your phone number");
+    		next = "/registration.jsp";
+    	}
+    	if (zip == ""){
+    		request.setAttribute("zip_err", "please enter your zip code");
+    		next = "/registration.jsp";
+    	}
+    	
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher(next);
+		
+    	try {
+    		dispatch.forward(request,response);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
