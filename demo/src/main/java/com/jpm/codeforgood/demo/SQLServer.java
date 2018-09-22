110a8a83fdc7afb5791c60f8ed2daeeb70173bd4
Ryan Kim <ryankim360@gmail.com>
2018-09-21 21:56:07 -0400

    Added SQLServer.java

--- demo/src/main/java/com/jpm/codeforgood/demo/SQLServer.java
+++ demo/src/main/java/com/jpm/codeforgood/demo/SQLServer.java
@@ -9,11 +9,14 @@ package com.jpm.codeforgood.demo;
  *
  * @author ryankim
  */
+
 import java.sql.Connection; 
 import java.sql.DriverManager; 
 import java.sql.ResultSet;
 import java.sql.SQLException; 
 import java.sql.Statement; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SQLServer {
     static final String JDBC_DRIVER = "org.h2.Driver";   
@@ -23,7 +26,34 @@ public class SQLServer {
     static final String USER = "sa"; 
     static final String PASS = ""; 
     
-    Connection conn = null; 
-    Statement stmt = null; 
+    static Connection conn = null; 
+    static Statement stmt = null; 
+    
+    /**
+     *
+     */
+    public static void startServer() {
+        try {
+            // STEP 1: Register JDBC driver 
+            Class.forName(JDBC_DRIVER);
+            
+            // STEP 2: Open a DS connection
+            System.out.println("Connecting to database..."); 
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);  
+            
+            //STEP 3: Execute a query 
+            System.out.println("Creating table in given database..."); 
+            stmt = conn.createStatement();
+                
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SQLServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
     
 }
