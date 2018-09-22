d5958f782d322c7961e98c5c79e606a1a4ba00b7
Ryan Kim <ryankim360@gmail.com>
2018-09-21 22:51:53 -0400

    Added CREATE TABLEs to populateTables();

--- demo/src/main/java/com/jpm/codeforgood/demo/SQLServer.java
+++ demo/src/main/java/com/jpm/codeforgood/demo/SQLServer.java
@@ -15,6 +15,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException; 
 import java.sql.Statement; 
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,7 +31,7 @@ public class SQLServer {
     static Statement stmt = null; 
     
     /**
-     *
+     *This method creates a connection to a local server.
      */
     public static void startServer() {
         try {
@@ -40,17 +41,77 @@ public class SQLServer {
             // STEP 2: Open a DS connection
             System.out.println("Connecting to database..."); 
             conn = DriverManager.getConnection(DB_URL,USER,PASS);  
-            
-            //STEP 3: Execute a query 
-            System.out.println("Creating table in given database..."); 
-            stmt = conn.createStatement();
                 
-            
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(SQLServer.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(SQLServer.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public static void createTables() throws SQLException {
+        //STEP 3: Execute a query 
+            System.out.println("Creating table in given database..."); 
+            stmt = conn.createStatement();
+            stmt.execute("DROP TABLE IF EXISTS USERS");
+            stmt.execute("DROP TABLE IF EXISTS ITEMS");
+            stmt.execute("DROP TABLE IF EXISTS  DONATE");
+            
+            String sql = "CREATE TABLE USERS " + 
+                    "(user_id INTEGER, " + 
+                    " name VARCHAR(255), " +   
+                    " age INTEGER, " +  
+                    " gender VARCHAR(15), "+
+                    " email_address VARCHAR(255), "+
+                    " phone_number INTEGER, "+
+                    " zipcode INTEGER, "+
+                    " user_credit INTEGER, "+
+                    " PRIMARY KEY ( user_id ))"; 
+            stmt.executeUpdate(sql);
+            
+            sql =  "CREATE TABLE ITEMS " + 
+                    "(item_id INTEGER, " + 
+                    " item_type VARCHAR(255), " +  
+                    " item_price DECIMAL(8,2), " +  
+                    " item_quantity INTEGER, " +  
+                    "item_description VARCHAR(255)," +
+                    "item_name VARCHAR(255)," +
+                    " PRIMARY KEY ( item_id ))"; 
+            stmt.executeUpdate(sql);
+            
+            sql = "CREATE TABLE DONATE "+
+            		"(donate_id INTEGER, "+
+            		"donation_date DATE, "+
+            		"amount_donated INTEGER, " +
+            		"location INTEGER, " +
+                        "user_id INTEGER," +
+                        "item_id INTEGER, " +
+            		"FOREIGN KEY(user_id) REFERENCES USERS(user_id), " +
+            		"FOREIGN KEY(item_id) REFERENCES ITEMS(item_id))";
+            stmt.executeUpdate(sql);
+            
+            
+    }
+    
+    public static void populateDatabase() throws SQLException {
+        
+        
+        //Holder variables for the input
+        int intHolder = 0;
+        String stringHolder = "";
+        
+        //Using JSON for inputting data into tables
+
+        
+        
+            
+    }
+    
+    public static void closeDatabase() throws SQLException {
+        // STEP 4: Clean-up environment 
+            stmt.close(); 
+            conn.close(); 
+            System.out.println("Goodbye!");
     }
     
     
